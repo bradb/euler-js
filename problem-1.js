78c8c3fb973ae0b34bc40140896de099b435c139
Brad Bollenbach <bradb@jobready.com.au>
2016-05-01 13:29:51 +1000

    Solution to problem 1

---
+++ problem-1.js
@@ -0,0 +1,13 @@
+var problem1 = function(max) {
+  var sum = 0;
+
+  for (var i = 0; i < max; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum += i;
+    }
+  }
+
+  return sum;
+}
+
+console.log(problem1(1000));
