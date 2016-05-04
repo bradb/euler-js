539ea0d4f7458c976bea208fd3640c7acbc314f8
Brad Bollenbach <bradb@jobready.com.au>
2016-05-04 20:49:34 +1000

    Solution to problem 4

---
+++ problem-4.js
@@ -0,0 +1,23 @@
+var largestPalindrome = function() {
+  'use strict'
+
+  var permutations = [];
+  var numString;
+  var x;
+  var y;
+
+  for (x = 100; x <= 999; x++) {
+    for (y = 100 + (x - 100); y <= 999; y++) {
+      permutations.push(x * y);
+    }
+  }
+
+  return permutations.
+    sort(function(a, b) { return b - a; }).
+    find(function(val) {
+      numString = val.toString();
+      return numString === numString.split('').reverse().join('');
+    })
+}
+
+console.log(largestPalindrome());
